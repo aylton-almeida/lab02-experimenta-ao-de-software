f1eb3d768294871f92feb5358a861e58be9c5e6a
Aylton Almeida <ayltonalmeida2@gmail.com>
2021-03-18 09:27:36 -0300

    half[+]progress[^]

--- clone.py
+++ clone.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import progressbar
 import git
+import sys
 
 from src.utils.csv import get_ck_data, save_repos_to_csv
 from src.models.Repo import Repo
@@ -18,49 +19,44 @@ def clone_repo():
     os.system('rm -rf repos/* ck_data.csv')
     os.system('touch ck_data.csv')
 
-    # extremely necessary progress bar for better user experience
-    widgets = [
-        progressbar.Percentage(),
-        progressbar.Bar(marker='\x1b[32m#\x1b[39m'),
-    ]
-    bar = progressbar.ProgressBar(
-        widgets=widgets,
-        max_value=len(data_frame.index),
-        min_value=0,
-        redirect_stdout=True
-    ).start()
-
     print('Analyzing repos...')
 
-    for index, row in data_frame.iterrows():
-        repo = Repo.from_dataframe(row)
-        repo_folder = repo.nameWithOwner.replace('/', '-')
+    data_arr = [*data_frame.iterrows()]
+    half = len(data_arr) // 2
 
-        print('Cloning repo {}...'.format(repo.nameWithOwner))
-        os.system("mkdir -p repos/{}".format(repo_folder))
-        git.Git("repos/{}".format(repo_folder)).clone(repo.url)
+    # Get first or second half
+    half_arr = data_arr[half:] if len(sys.argv) == 1 else data_arr[:half]
 
-        print('Running CK...')
-        os.system(
-            "java -jar ck/target/ck-0.6.4-SNAPSHOT-jar-with-dependencies.jar repos/{} true 0 false".format(
-                repo_folder))
+    # extremely necessary progress bar for better user experience
+    with progressbar.ProgressBar(max_value=len(data_frame.index), redirect_stdout=True) as bar:
+        for index, row in half_arr:
+            repo = Repo.from_dataframe(row)
+            repo_folder = repo.nameWithOwner.replace('/', '-')
 
-        print('Renaming files...')
-        os.system(
-            'mv class.csv repos/{}/class.csv'.format(repo_folder))
+            print('Cloning repo {}...'.format(repo.nameWithOwner))
+            os.system("mkdir -p repos/{}".format(repo_folder))
+            git.Git("repos/{}".format(repo_folder)).clone(repo.url)
 
-        repo.add_ck_data(get_ck_data('repos/{}/class.csv'.format(repo_folder)))
-        save_repos_to_csv([repo], 'ck_data.csv', 'a')
+            print('Running CK...')
+            os.system(
+                "java -jar ck/target/ck-0.6.4-SNAPSHOT-jar-with-dependencies.jar repos/{} true 0 false".format(
+                    repo_folder))
 
-        # remove method.csv after finishing
-        print('Deleting method.csv...'.format(repo.nameWithOwner))
-        os.system('rm -rf method.csv')
+            print('Renaming files...')
+            os.system(
+                'mv class.csv repos/{}/class.csv'.format(repo_folder))
 
-        bar.update(index + 1)
+            repo.add_ck_data(get_ck_data(
+                'repos/{}/class.csv'.format(repo_folder)))
+            save_repos_to_csv([repo], 'ck_data.csv', 'a')
+
+            # remove method.csv after finishing
+            print('Deleting method.csv...'.format(repo.nameWithOwner))
+            os.system('rm -rf method.csv')
+
+            bar.update(index + 1)
 
     print('All repos were fetched')
-
-    bar.finish()
 
 
 if __name__ == '__main__':
