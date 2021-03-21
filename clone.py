07916be8dfd70082cf59bd7a2fa00e0283d5dc53
Ubuntu <ubuntu@ip-172-31-44-203.us-east-2.compute.internal>
2021-03-21 14:28:00 +0000

    aws

--- clone.py
+++ clone.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import progressbar
+import git
 
 from src.utils.csv import get_ck_data, save_repos_to_csv
 from src.models.Repo import Repo
@@ -28,27 +29,30 @@ def clone_repo():
     with progressbar.ProgressBar(max_value=len(half_arr), redirect_stdout=True) as bar:
         for index, row in half_arr:
             repo = Repo.from_dataframe(row)
-            repo_folder = repo.nameWithOwner.replace('/', '-')
+            try:
+                repo_folder = repo.nameWithOwner.replace('/', '-')
 
-            print('Cloning repo {}...'.format(repo.nameWithOwner))
-            os.system("mkdir -p repos/{}".format(repo_folder))
-            os.system('git clone {} repos/{}/{}'.format(repo.url, repo_folder,
-                                                        repo.nameWithOwner.split('/')[1]))
+                print('Cloning repo {}...'.format(repo.nameWithOwner))
+                os.system("mkdir -p repos/{}".format(repo_folder))
+                git.Git("repos/{}".format(repo_folder)).clone(repo.url)
 
-            print('Running CK...')
-            os.system(
-                "java -jar ck/target/ck-0.6.4-SNAPSHOT-jar-with-dependencies.jar repos/{} true 0 false".format(
-                    repo_folder))
+                print('Running CK...')
+                os.system(
+                    "java -jar ck/target/ck-0.6.4-SNAPSHOT-jar-with-dependencies.jar repos/{} true 0 false".format(
+                        repo_folder))
 
-            repo.add_ck_data(get_ck_data(
-                'class.csv'.format(repo_folder)))
-            save_repos_to_csv([repo], 'ck_data.csv', 'a')
+                repo.add_ck_data(get_ck_data(
+                    'class.csv'.format(repo_folder)))
+                save_repos_to_csv([repo], 'ck_data.csv', 'a')
 
-            # remove method.csv after finishing
-            print('Deleting method.csv...'.format(repo.nameWithOwner))
-            os.system('rm -rf method.csv class.csv repos/*')
-
-            bar.update(index + 1)
+                # remove method.csv after finishing
+                print('Deleting method.csv...'.format(repo.nameWithOwner))
+                os.system('rm -rf method.csv class.csv repos/*')
+            except:
+                save_repos_to_csv([repo], 'extra_data.csv', 'a')
+            finally:
+                bar.update(index)
+                
 
     print('All repos were fetched')
 
